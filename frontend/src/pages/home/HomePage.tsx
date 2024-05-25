476ca16a553f6f9c50091fabe68aa6b56cb3c3ed
hendrahartanto <hendrazhuo10@gmail.com>
2024-05-25 14:57:30 +0700

    add hero section in homepage

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,9 +1,23 @@
-import PromoCarousel from "./components/PromoCarousel";
+import heroDogImage from "../../assets/dog_picture/dog_picture1.png";
 
 const HomePage = () => {
 	return (
 		<div className="home-page">
-			<PromoCarousel />
+			<div className="hero-section">
+				<div className="page-center">
+					<div className="hero-container">
+						<div className="hero-left">
+							<p className="title">Welcome to Happy Paws</p>
+							<p className="subtitle">
+								The Best Place to Care for Your Pets with Love and Attention
+							</p>
+						</div>
+						<div className="hero-right">
+							<img src={heroDogImage} alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
